6339c9f74dc35962b7152977a37d971066497aa7
alexandrahufnagel <alexandra.hufnagel@gmx.de>
2021-03-06 21:53:47 +0100

    step14Leson4

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet var imagePickerView: UIImageView!
     @IBOutlet var cameraButton: UIBarButtonItem!
@@ -24,24 +24,84 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         NSAttributedString.Key.strokeWidth: -3.0
     ]
  
-        
     override func viewDidLoad() {
         super.viewDidLoad()
 
         textFieldTop.text = "TOP"
         textFieldBottom.text = "BOTTOM"
         
-      //  self.textFieldTop.delegate = self.topDelegate
-      //    self.textFieldBottom.delegate = self.bottomDelegate
+        textFieldTop.delegate = self
+        textFieldBottom.delegate = self
+      
         self.textFieldTop.defaultTextAttributes = memeTextAttributes
         self.textFieldBottom.defaultTextAttributes = memeTextAttributes
+        
         textFieldTop.textAlignment = .center
         textFieldBottom.textAlignment = .center
     }
     
     override func viewWillAppear(_ animated: Bool) {
            cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
        }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" {
+            textField.text = ""
+        }
+        if textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textFieldTop.text == "" {
+            textFieldTop.text = "TOP"
+        }
+        if textFieldBottom.text == "" {
+            textFieldBottom.text = "BOTTOM"
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Mark: keyboard
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if textFieldBottom.isEditing {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+                }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if textFieldBottom.isEditing {
+                    view.frame.origin.y = 0
+                }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name:  UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
         presentImagePickerWith(.photoLibrary)
