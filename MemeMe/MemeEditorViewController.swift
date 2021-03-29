41463d8bcebb960259b6386a2e4c26472ec7e976
alexandrahufnagel <alexandra.hufnagel@gmx.de>
2021-03-29 13:15:15 +0200

    just suggestions implemented

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -20,10 +20,10 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet var cancelButton: UIButton!
     
     let memeTextAttributes: [NSAttributedString.Key : Any] = [
-        NSAttributedString.Key.strokeColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth: -3.0
+            .strokeColor: UIColor.black,
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            .strokeWidth: -3.0
     ]
         
     override func viewDidLoad() {
